4b533444be978ce35002558a8335a1a579eddd55
Ignacio de Miguel Diaz <imigueldiaz@gmail.com>
2023-11-15 11:00:15 +0100

    Update OpenAIHelper.js
    Signed-off-by: Ignacio de Miguel Diaz <imigueldiaz@gmail.com>

--- helpers/OpenAIHelper.js
+++ helpers/OpenAIHelper.js
@@ -1,31 +1,29 @@
 const fs = require('fs');
-const {createCompletion} = require('openai');
+const OpenAI = require('openai');
 
 class OpenAIHelper {
-	static systemMessage = fs.readFileSync('system.txt', 'utf-8');
-	
-	constructor(apiKey) {
-		this.apiKey = apiKey;
-	}
+    static systemMessage = fs.readFileSync('system.txt', 'utf-8');
 
-	async search(userMessage, choices) {
-		const response = await createCompletion({
-			apiKey: this.apiKey,
-			model: 'gpt-3.5-turbo-0613',
-			messages: [
-				{role: 'system', content: OpenAIHelper.systemMessage},
-				{role: 'user', content: userMessage},
-			],
-			temperature: 0.16,
-			max_tokens: 120,
-			frequency_penalty: 0.1,
-			presence_penalty: 0.1,
-			n: choices,
-		});
+    constructor(apiKey) {
+        this.openai = new OpenAI(apiKey);
+    }
 
-		return response;
-	}
+    async search(userMessage, choices) {
+        const response = await this.openai.createChatCompletion({
+            model: 'gpt-3.5-turbo-0613',
+            messages: [
+                {role: 'system', content: OpenAIHelper.systemMessage},
+                {role: 'user', content: userMessage},
+            ],
+            temperature: 0.16,
+            max_tokens: 120,
+            frequency_penalty: 0.1,
+            presence_penalty: 0.1,
+            n: choices,
+        });
+
+        return response;
+    }
 }
 
 module.exports = OpenAIHelper;
-
